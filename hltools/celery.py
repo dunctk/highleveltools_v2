c3afa7b194d15a083465e7b1d4fdf935a336fcb6
Duncan Felix <d@uncan.net>
2024-07-29 08:34:11 +0200

    x

--- hltools/celery.py
+++ hltools/celery.py
@@ -1,12 +1,3 @@
-import os
-from celery import Celery
+from .celery_app import app as celery_app
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hltools.settings')
-
-app = Celery('hltools')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-# Ensure this import is at the end of the file
-from sync.periodic_tasks import setup_periodic_tasks
-setup_periodic_tasks(app)
+__all__ = ('celery_app',)
