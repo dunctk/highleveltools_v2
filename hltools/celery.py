c4c8393985dbbe442af4a9a7bb2044d72809d2f6
Duncan Felix <d@uncan.net>
2024-07-29 08:29:28 +0200

    x

--- hltools/celery.py
+++ hltools/celery.py
@@ -6,3 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hltools.settings')
 app = Celery('hltools')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Ensure this import is at the end of the file
+from sync.periodic_tasks import setup_periodic_tasks
+setup_periodic_tasks(app)
--- sync/periodic_tasks.py
+++ sync/periodic_tasks.py
@@ -1,33 +1,14 @@
-from celery import shared_task, app
+from celery import shared_task
 from django.core.management import call_command
 from celery.schedules import crontab
-from django_celery_beat.models import PeriodicTask, CrontabSchedule
 
 @shared_task(name='sync.run_sync_script')
 def run_sync_script():
     call_command('runscript', 'sync')
 
-@app.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
+def setup_periodic_tasks(app):
+    app.add_periodic_task(
         crontab(minute=0, hour='*/1'),
         run_sync_script.s(),
         name='Run sync script hourly'
     )
-
-# def setup_periodic_tasks():
-#     schedule, _ = CrontabSchedule.objects.get_or_create(
-#         minute='0',
-#         hour='0',
-#         day_of_week='*',
-#         day_of_month='*',
-#         month_of_year='*',
-#     )
-#     PeriodicTask.objects.get_or_create(
-#         name='Run sync script hourly',
-#         task='sync.periodic_tasks.run_sync_script',
-#         crontab=schedule,
-#     )
-
-# # Call this function somewhere in your app's initialization
-# # setup_periodic_tasks()
