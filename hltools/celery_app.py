013f0b20a19297831c5943630bdbcd3dfaec0204
Duncan Felix <d@uncan.net>
2024-07-29 21:12:32 +0200

    x

--- hltools/celery_app.py
+++ hltools/celery_app.py
@@ -6,7 +6,6 @@ from celery.schedules import crontab
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azureproject.settings')
 
 settings_module = 'hltools.settings'
 
