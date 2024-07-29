497077b5269f431c33ae49742c491a12a2a996e2
Duncan Felix <d@uncan.net>
2024-07-29 08:19:20 +0200

    x

--- sync/celery.py
+++ sync/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hltools.settings')
 
-app = Celery('your_project')
+app = Celery('hltools')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
