0ae92eafd13937b772879b0e4da36a7253127949
Duncan Felix <d@uncan.net>
2024-07-31 07:23:19 +0200

    x

--- hltools/settings.py
+++ hltools/settings.py
@@ -139,3 +139,4 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'  # Set this to your project's timezone
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
